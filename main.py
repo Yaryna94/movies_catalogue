d17debbdff1b66485a398e8df835cdbf98c6d4ca
Yaryna94 <120921160+Yaryna94@users.noreply.github.com>
2023-02-26 19:45:43 +0300

    Add files via upload
    deferent

--- main.py
+++ main.py
@@ -2,25 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 import tmdb_client
-
-# @app.route('/')
-# def homepage():
-#     movies = [movie for movie in range(0, 8)]
-#     return render_template("homepage.html", movies=movies)
-
-
+import random
+from flask import request
 
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_popular_movies()["results"][:8]
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get('list_type', "popular")
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list)
+
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    movie_images = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_images['backdrops'])
+    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
 
 @app.context_processor
 def utility_processor():
     def tmdb_image_url(path: str, size: str):
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
+
 
 
 if __name__ == '__main__':
